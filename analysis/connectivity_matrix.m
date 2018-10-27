2df889db04a95ec3d25be896e79d732a83d2ce67
rgbayrak <rozgns@gmail.com>
2018-10-26 23:35:53 -0500

    Update connectivity_matrix.m

--- analysis/connectivity_matrix.m
+++ analysis/connectivity_matrix.m
@@ -6,8 +6,8 @@ clear all
 close all
 
 %%
-seg = load_nii('/share5/bayrakrg/tractEM/postprocessing/M-tracts/1134_BLSA/SEG/wcrBLSA_1134_16-0_10_MPRAGE_seg.nii');
-density = load_nii('/share5/bayrakrg/tractEM/postprocessing/M-tracts/1134_BLSA/posterior_limb_internal_capsule/pic_R_density.nii.gz');
+seg = load_nii('pathtofile/wcrBLSA_1134_16-0_10_MPRAGE_seg.nii');
+density = load_nii('pathtofile/pic_R_density.nii.gz');
 
 % Brain region labels
 BrainColorROI = [4 11 23 30 31 32 35 36 37 38 39 40 41 44 45 47 48 49 50 51 52 55 56 57 58 59 60 61 62 71 72 73 75 76 100 101 102 103 104 105 106 107 108 109 112 113 114 115 116 117 118 119 120 121 122 123 124 125 128 129 132 133 134 135 136 137 138 139 140 141 142 143 144 145 146 147 148 149 150 151 152 153 154 155 156 157 160 161 162 163 164 165 166 167 168 169 170 171 172 173 174 175 176 177 178 179 180 181 182 183 184 185 186 187 190 191 192 193 194 195 196 197 198 199 200 201 202 203 204 205 206 207];
@@ -46,7 +46,7 @@ xlabel('Brain Color ROI')
 colormap(parula(100000)); % set the colorscheme
 hcb = colorbar;
 hcb.Label.String = 'Correlation';
-saveas(p,'/home/local/VANDERBILT/bayrakrg/Desktop/pic_R.jpg'); 
+saveas(p,'pathtofile/pic_R.jpg'); 
 
 
 
