e9ec117573b51a94e4fd7134412c2abb2b71bfa8
rgbayrak <rozgns@gmail.com>
2019-06-27 11:53:44 -0500

    Remove duplicated directory

--- analysis/analysis/hausdorff_calc/HD.m
+++
@@ -1,9 +0,0 @@
-function [hd, hdmean, modHausdd, hd90] = HD(A, B)
-    % HD, HDmean, HD90
-    [a_x, a_y, a_z] = ind2sub(size(A), find(A));
-    a_coords = cat(2, a_x, a_y, a_z);
-    [b_x, b_y, b_z] = ind2sub(size(B), find(B));
-    b_coords = cat(2, b_x, b_y, b_z); 
-    clear a_x a_y a_z b_x b_y b_z
-    [hd, hdmean, modHausdd, hd90] = HausdorffDists(a_coords,b_coords);
-end
